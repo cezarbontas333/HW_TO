8bec78ed0224528e86b7849129bf7485557828fd
cezarbontas333 <cezar.bontas@yahoo.com>
2023-05-25 23:43:28 +0300

    Fixed unicode characters for rendering text in LaTeX.

--- CalcTir.jl
+++ CalcTir.jl
@@ -16,15 +16,15 @@ function CalcTir(Vec_Jucarii::Vector{Jucarie}, Vec_Tiruri::Vector{Tir})::Vector{
         IC = Model(HiGHS.Optimizer)
 
         # Variabilele functiei de optimizat, adica numarul de jucarii transportate
-        @variable(IC, Cantitate[index_jucarie=1:Nr_Jucarii] ≥ 0, Int)
+        @variable(IC, Cantitate[index_jucarie=1:Nr_Jucarii] >= 0, Int)
 
         # Functia obiectiv; optimizarea profitului maxim pentru toate tipurile de jucarii
         @objective(IC, Max, sum(Vec_Jucarii[index_jucarie].Profit * Cantitate[index_jucarie] for index_jucarie = 1:Nr_Jucarii))
 
         # Contrangerile: de greutate, respectiv de volum, cat si canticatea minima ce trebuie vanduta
-        @constraint(IC, sum(Vec_Jucarii[index_jucarie].Greutate * Cantitate[index_jucarie] for index_jucarie = 1:Nr_Jucarii) ≤ Vec_Tiruri[index_tir].Greutate)
-        @constraint(IC, sum(Vec_Jucarii[index_jucarie].Volum * Cantitate[index_jucarie] for index_jucarie = 1:Nr_Jucarii) ≤ Vec_Tiruri[index_tir].Volum)
-        @constraint(IC, [index_jucarie = 1:Nr_Jucarii], Cantitate[index_jucarie] ≥ Vec_Jucarii[index_jucarie].Cantitate_Min)
+        @constraint(IC, sum(Vec_Jucarii[index_jucarie].Greutate * Cantitate[index_jucarie] for index_jucarie = 1:Nr_Jucarii) <= Vec_Tiruri[index_tir].Greutate)
+        @constraint(IC, sum(Vec_Jucarii[index_jucarie].Volum * Cantitate[index_jucarie] for index_jucarie = 1:Nr_Jucarii) <= Vec_Tiruri[index_tir].Volum)
+        @constraint(IC, [index_jucarie = 1:Nr_Jucarii], Cantitate[index_jucarie] >= Vec_Jucarii[index_jucarie].Cantitate_Min)
 
         # Optimizarea si afisarea rezultatelor
         optimize!(IC)
